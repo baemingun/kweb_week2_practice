4c635818d946b0148fae1f7ed0b522282bd49359
unknown <baemingun@naver.com>
2018-10-01 17:25:52 +0900

    index.js readFileSync 파일 수정

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const hostname = '127.0.0.1';
 const port = 3000;
 
 const server = http.createServer((req, res) => {
-    const indexContent = fs.readFileSync(path.join(__dirname,'index.html'));
+    const indexContent = fs.readFileSync(path.join(__dirname,'practice.txt'));
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain; charset=uft-8');
     res.end(indexContent);
